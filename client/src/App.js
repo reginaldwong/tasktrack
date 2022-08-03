e546d24a6e684dff43250ce68b03b89df76f92a6
Reginald Wong <reginald_wong@outlook.com>
2022-08-03 09:25:48 -0400

    set up react router and 404 page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import Clients from './components/Clients';
-import Projects from './components/Projects';
-import AddClientModal from './components/AddClientModal';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const cache = new InMemoryCache({
@@ -32,12 +32,15 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
-        <Header />
-        <div className="container">
-          <AddClientModal />
-          <Projects />
-          <Clients />
-        </div>
+        <Router>
+          <Header />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </Router>
       </ApolloProvider>
     </>
   );
--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -1,6 +1,6 @@
 export default function ProjectCard({ project }) {
   return (
-    <div className="col-md-4">
+    <div className="col-md-6">
       <div className="card mb-3">
         <div className="card-body">
           <div className="d-flex justify-content-between align-items center">
