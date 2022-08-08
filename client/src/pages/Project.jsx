e6b821dc12ceb8753be3e770cb1f4abf3b746882
Reginald Wong <reginald_wong@outlook.com>
2022-08-08 09:06:16 -0400

    add update project mutation to front end

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -2,6 +2,7 @@ import { Link, useParams } from 'react-router-dom';
 import Spinner from '../components/Spinner';
 import ClientInfo from '../components/ClientInfo';
 import DeleteProjectButton from '../components/DeleteProjectButton';
+import EditProjectForm from '../components/EditProjectForm';
 import { useQuery } from '@apollo/client';
 import { GET_PROJECT } from '../queries/projectQueries';
 
@@ -26,7 +27,7 @@ export default function Project() {
           <p className="lead">{data.project.status}</p>
 
           <ClientInfo client={data.project.client} />
-
+          <EditProjectForm project={data.project} />
           <DeleteProjectButton projectId={data.project.id} />
         </div>
       )}
