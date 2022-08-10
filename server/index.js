7b3a214c3aabca578ceb22efa70c72e0200e7453
Reginald Wong <reginald_wong@outlook.com>
2022-08-10 15:38:16 -0400

    update

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const colors = require('colors');
 const cors = require('cors');
-require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const connectDB = require('./config/db');
